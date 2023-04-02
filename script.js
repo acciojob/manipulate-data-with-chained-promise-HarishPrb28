5194f7036418b73e31789a7a786b3d65da2ed536
Harish  Parab <50201375+HarishPrb28@users.noreply.github.com>
2023-04-02 23:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function manipulateArray(array){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			//Odd Array
+			const filterArray = array.filter((num) => num%2 === 0)
+			setTimeout(()=>{
+			document.getElementById("output").innerText = filterArray.join(",");
+			//multiply num by 2
+			const multiplyArray = filterArray.map((num)=> num*2)
+			setTimeout(()=>{
+			document.getElementById("output").innerText = multiplyArray.join(",");
+			resolve(multiplyArray)
+			},2000)
+			},1000)
+		},3000)
+	})
+}
+
+
+manipulateArray([1,2,3,4]).then((result)=>{
+	console.log(result)
+})
